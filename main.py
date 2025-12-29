fe05a3604824a6bd7b48095ea190369de8645537
Daniel Marcos Pires <daniel.pires@gpnet.com.br>
2025-12-29 13:17:33 -0300

    Em desenvolvimento

--- main.py
+++ main.py
@@ -8,19 +8,30 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         list1 = list(l1)
+        list1.reverse()
         listInt1 = [int(x) for x in list1]
         list2 = list(l2)
+        list2.reverse()
         listInt2 = [int(y) for y in list2]
-        sumList = [x+y for x,y in zip(listInt1,listInt2)]
-        print(listInt1)
-        print(listInt2)
         
-        print(sumList)
+        print(f"l1 = {listInt1}")
+        print(f"l2 = {listInt2}")
+        
+        sumList = [x+y for x,y in zip(listInt1,listInt2)]
+        
+        for value in sumList:
+            print(value)
+            if(value >= 10):
+                separator = [x for x in str(value)]
+                print(f"list={separator} length={separator.__len__()}")
+
         
 
 solution = Solution()
 
 Inp1 = input("l1 = ")
-Inp2 = input("l1 = ")
+Inp2 = input("l2 = ")
+#243
+#564
 
 solution.addTwoNumbers(Inp1,Inp2)
