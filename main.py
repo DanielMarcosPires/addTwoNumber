916de1235b8dbb10982dfa39e3b106060c8292bf
Daniel Marcos Pires <daniel.pires@gpnet.com.br>
2025-12-30 13:33:38 -0300

    Atualizando

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functools import reduce
+custom = 40
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -15,23 +16,40 @@ class Solution(object):
         listInt2 = [int(y) for y in list2]
         
         print(f"l1 = {listInt1}")
-        print(f"l2 = {listInt2}")
+        print(f"l2 = {listInt2}\n")
         
         sumList = [x+y for x,y in zip(listInt1,listInt2)]
+        index = sumList.__len__()-1
         
-        for value in sumList:
-            print(value)
-            if(value >= 10):
-                separator = [x for x in str(value)]
-                print(f"list={separator} length={separator.__len__()}")
+        print(f"while 0 <={index}")
+        test = []
+        while 0<=index :
+            print(f"List = {sumList} index={index}")
+            if(sumList[index] > 9):
+                numeroSelecionado = sumList[index]
+                separador = [int(digito) for digito in str(numeroSelecionado)]
+                print(separador)
+                sumList[index] = separador[-1]
+                
+                if(index > 0 ):
+                 sumList[index-1] = sumList[index-1] + separador[0] 
+             
+            
+            index -= 1;
+        print(f"resposta = {sumList}")
+            
 
         
 
 solution = Solution()
-
+print("="*custom)
 Inp1 = input("l1 = ")
 Inp2 = input("l2 = ")
 #243
 #564
 
+print("-"*custom)
 solution.addTwoNumbers(Inp1,Inp2)
+print("-"*custom)
+
+print("="*custom)
